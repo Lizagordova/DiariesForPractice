18fbb7c11a99ba5d306d64161bcd87569ee194a7
elizaveta.gordova <Elizaveta.Gordova@Contractor.Prometric.com>
2021-05-10 12:09:31 +0300

    добавила новые компоненты для дневника

---
+++ DiariesForPractice/client/src/components/Student/HomePage/Diary/DiaryCompletion.tsx
@@ -0,0 +1,21 @@
+﻿import React, { Component } from "react";
+import { observer } from "mobx-react";
+
+class DiaryCompletionProps {
+
+}
+
+@observer
+class DiaryCompletion extends Component<DiaryCompletionProps> {
+    constructor(props: DiaryCompletionProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <></>
+        );
+    }
+}
+
+export default DiaryCompletion;
---
+++ DiariesForPractice/client/src/components/Student/HomePage/Diary/DiaryWindow.tsx
@@ -0,0 +1,21 @@
+﻿import React, { Component } from "react";
+import { observer } from "mobx-react";
+
+class DiaryWindowProps {
+    
+}
+
+@observer
+class DiaryWindow extends Component<DiaryWindowProps> {
+    constructor(props: DiaryWindowProps) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <></>
+        );
+    }
+}
+
+export default DiaryWindow;
